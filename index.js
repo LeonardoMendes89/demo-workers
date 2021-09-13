5ce78da7a23542bedcca4fa9b2dc6f223c89a922
Leozin89python <leozin069@gmail.com>
2021-09-13 06:00:16 -0300

    demo

--- index.js
+++ index.js
@@ -6,12 +6,13 @@ const db = require('./db/db')
 const port = process.env.PORT || 8006
 const app = express()
 
+app.db = db
+
 consign()
     .then('./config/expss.js')
     .then('./api')
     .then('./routes')
     .into(app)
 
-app.db = db
 
 app.listen(port,()=>console.log(`online into port: ${port}`))
