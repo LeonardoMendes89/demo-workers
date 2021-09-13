a5f9f9c6dc23aa43657643515802ac7a436d66a8
Leozin89python <leozin069@gmail.com>
2021-09-13 04:39:19 -0300

    demo

--- test/testRoute.js
+++ test/testRoute.js
@@ -17,14 +17,18 @@ testRouter.route('/').get(async(req,res)=>{
     if( !workername || !workerjob || !workersalary || !workersector ) return res.status(400).json({msg: " The fields value can't be empty! "})
    
     return await db('workeradmin').insert({ workername, workerjob, workersalary, workersector })
-                                  .then(_      => res.status(200).json({msg: 'data inserted with success!'}))
+                                  .then(_      => res.status(201).json({msg: 'data inserted with success!'}))
                                   .catch(err   => res.status(500).json(err) )
 
 })
 
 testRouter.route('/:id').delete(async(req,res)=>{
 
+    const { id } = req.body
 
+    return await db('workeradmin').where(id).delete()
+                                  .then(_      => res.status(204).json())
+                                  .catch(err   => res.status(500).json(err) )    
 
 }).put(async(req,res)=>{
 
