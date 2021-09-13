fce11832afdef1059d78cccb69c830b4409f1444
Leozin89python <leozin069@gmail.com>
2021-09-13 03:58:39 -0300

    demo

--- knexfile.js
+++ knexfile.js
@@ -17,9 +17,11 @@ module.exports = {
   production: {
     client: 'pg',
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      host:     'ec2-44-193-150-214.compute-1.amazonaws.com',
+      database: 'd6kse4d3jmav11',
+      user:     'ptjprhygkueelm',
+      password: 'ea8c6ab8e68ce46a8be94b25d2dd533d84dc31ec237e89763493c87439f9eed1',
+      ssl:       { rejectUnauthorized: false }
     }
   }
 
