e805efe3b87a2884bd75050045790e8e8c84ace8
Leozin89python <leozin069@gmail.com>
2021-09-13 06:12:52 -0300

    demo

--- api/index.js
+++ api/index.js
@@ -17,11 +17,31 @@ module.exports = app => {
 
         if( !workername || !workerjob || !workersalary || !workersector ) return res.status(400).json({msg: "The fields value can't be empty!"})
     
-        return await db('workeradmin').insert({ workername, workerjob, workersalary, workersector })
-                                     .then(_      => res.status(201).json({msg: 'data inserted with success!'}))
-                                     .catch(err   => res.status(500).json(err) )
+        return await db('workeradmin')
+                        .insert({ workername, 
+                                  workerjob, 
+                                  workersalary, 
+                                  workersector })
+                        .then(_      => res.status(201).json({msg: 'data inserted with success!'}))
+                        .catch(err   => res.status(500).json(err) )
+    }
+
+    const putData = async(req,res) => {
+        const { id } = req.query
+        const { workername, workerjob, workersalary, workersector } = req.body
+
+        if( !workername || !workerjob || !workersalary || !workersector ) return res.status(400).json({msg: "The fields value can't be empty!"})
+        
+        return await db('workeradmin')
+                        .where('id',id)
+                        .update({ workername, 
+                                  workerjob, 
+                                  workersalary, 
+                                  workersector })
+                        .then(_      => res.status(201).json({msg: 'data updated with success!'}))
+                        .catch(err   => res.status(500).json(err) )
     }
 
 
-return { getData , postData }
+return { getData , postData , putData }
 }
