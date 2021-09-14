f2c0fcbd6f6dbce57d600a794970f01865e4fac2
Leozin89python <leozin069@gmail.com>
2021-09-13 23:24:09 -0300

    demo

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 module.exports = app => {
     app.get('/',app.api.index.getData)
-    app.get('/:id',app.api.index.getDataByName)
+    app.get('/:id',app.api.index.getDataById)
+    app.get('/:name',app.api.index.getDataByName)
     app.post('/',app.api.index.postData)
     app.put('/?',app.api.index.putData)
     app.delete('/?',app.api.index.deleteData)
